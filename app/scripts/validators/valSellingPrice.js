3c48b2e190bae146f29d95b2629ed4279d403561
sujit13666 <sujit13666@gmail.com>
2017-01-27 11:46:02 +0600

    Euro Calculation, Video News Done & Spanish Concept Video Done

---
+++ app/scripts/validators/valSellingPrice.js
@@ -0,0 +1,35 @@
+﻿(function () {
+
+    'use strict';
+    app.directive("valEmail",valEmail);
+
+    valEmail.$inject=['$q','$timeout','securityService'];
+
+    function valEmail ($q, $timeout, securityService) {
+        return {
+            restrict: "A",
+            require: "ngModel",
+            link: function($scope, $element, $attrs, ctrl) {
+
+                ctrl.$asyncValidators.valEmail = function(modelValue, viewValue) {
+
+                    if (ctrl.$isEmpty(modelValue)) {
+                        // consider empty model valid
+                        return $q.when();
+                    }
+
+                    var def = $q.defer();
+
+                    securityService.checkIfEmailExist({'query':modelValue}).then(function(response){
+                        if(response.data.success.emailExist)def.reject();
+                            else def.resolve();
+                    });
+
+
+                    return def.promise;
+                };
+            }
+        };
+    }
+
+})();
