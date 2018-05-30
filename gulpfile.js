eab3d17f2269d72df769dcbda18b30ac998e7162
sujit13666 <sujit.bit.0329@gmail.com>
2018-05-30 22:12:06 +0600

    Fixed- Lowest Online Book Deal Price with Shipping Price

--- gulpfile.js
+++ gulpfile.js
@@ -299,7 +299,7 @@ gulp.task("js_app", function (done) {
         .pipe(plugins.uglify({
             mangle: true
         }))
-        .pipe(plugins.concat('app-es-0.17.min.js'))
+        .pipe(plugins.concat('app-es-0.18.min.js'))
         .pipe(gulp.dest('app/dist/js'));
 });
 gulp.task("css_website", function (done) {
